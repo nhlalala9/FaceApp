3debbb25977a60c98539639d5cf5badeaefda937
Nhlalala <nhlalala.mudanisi@dadevs.co.za>
2023-02-15 14:37:56 +0200

    topbarr

--- frontend/src/topbar/Topbar.jsx
+++ frontend/src/topbar/Topbar.jsx
@@ -6,7 +6,7 @@ export default function Topbar() {
     return (
         <div className="topbarContainer">
             <div className="topbarLeft">
-                <span className="logo">NewFacebook</span>
+                <img src="/assets/facebook.png" alt="" className="topbarImg"/>
             </div>
             <div className="topbarCentre">
                 <div className="searchbar">
