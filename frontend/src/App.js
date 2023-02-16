695ec90fc69874d42fab1c2a68a98f591e8cda36
Nhlalala <nhlalala.mudanisi@dadevs.co.za>
2023-02-16 09:47:35 +0200

    addeddd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,22 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import Feed from "./components/feed/Feed";
 import Rightbar from "./components/rightbar/Rightbar";
+import Profile from "./pages/profile/Profile.jsx";
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+  return ( 
+    <>
       <Topbar />
-      <Sidebar />
-      {/* <Feed /> */}
-      <Rightbar />
-    </div>
+      <div className="App">
+
+        <Sidebar />
+        {/* <Feed /> */}
+        <Rightbar />
+        {/* <Profile/> */}
+        
+      </div>
+    </>
 
   );
 }
--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -3,18 +3,19 @@ import Topbar from "../../components/topbar/Topbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Feed from "../../components/feed/Feed";
 import Rightbar from "../../components/rightbar/Rightbar";
+import Profile from "./pages/profile/Profile.jsx";
 import "./Home.css"
 
 
 export default function Home() {
-    return (
-        <>
-        <Topbar/>
-        <div className="homeContainer">
-            <Sidebar/>
-            <Feed/>
-            <Rightbar/>
-        </div>
-        </>
+    return ( <Profile/>
+        // <>
+        // <Topbar/>
+        // <div className="homeContainer">
+        //     <Sidebar/>
+        //     <Feed/>
+        //     <Rightbar/>
+        // </div>
+        // </>
     )
 }
